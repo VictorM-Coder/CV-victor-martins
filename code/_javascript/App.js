843ea9b7a18a8135b5a15c72c06d55be8d497f6f
Victor Martins <martinsvictor6781@gmail.com>
2022-05-02 15:57:48 -0300

    adição do servidor local para testes

--- code/_javascript/App.js
+++ code/_javascript/App.js
@@ -1,10 +1,20 @@
-for (const index in section) {
-    let container = new Container(section[index])
-    for (const index1 in section[index].items) {
-        container.addItem(section[index].items[index1])
+const ajax = new XMLHttpRequest()
+ajax.open('GET', 'http://localhost:3000/section')
+
+ajax.onreadystatechange = () => {
+    if(ajax.readyState == 4 && ajax.status == 200){
+        let section = JSON.parse(ajax.responseText)
+        for (const index in section) {
+            let container = new Container(section[index])
+            for (const index1 in section[index].items) {
+                container.addItem(section[index].items[index1])
+            }
+            document.getElementById('section' + index).appendChild(container.getContainer())
+        }
     }
-    document.getElementById('section' + index).appendChild(container.getContainer())
 }
+
+ajax.send()
 
 let addressContatos = new ContainerFooter()
 addressContatos.addItem('github', 'mdi:github', '')
